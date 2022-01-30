b60f7f73bb8a0182ce28452dce4d608493ea4f93
tochy <ukwuomatochi2@gmail.com>
2022-01-30 20:44:14 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const gif = document.querySelector('#gif')
+const about = document.querySelector('#about')
+
+// gif.document.style.cursor ='pointer';
+gif.addEventListener('click', function(){
+    position.assign('#about')
+})
